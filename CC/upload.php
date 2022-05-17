c925d2d388fea53117c2023683386009e7fb7893
rifkikautsar <rifkikautsar1@gmail.com>
2022-05-17 20:14:12 +0700

    re-route folder for MD, ML, CC

--- CC/upload.php
+++ CC/upload.php
@@ -8,6 +8,9 @@ if(isset($_FILES["upload"])){
     if(!@$image->name){
         array_push($errorMsg, "Upload Your Image First");
     }
+    if($image->size > 1000*10000){
+        array_push($errorMsg, "Maximum Image Size 10 Mb");
+    }
     echo "Upload Success";
     echo "<pre>";
     // var_dump($_FILES);
